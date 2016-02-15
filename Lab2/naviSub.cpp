968ae95f940cb49243ded89b7bc44620c0437fb0
Matthew <mdombros@u.rochester.edu>
2016-02-15 16:57:25 -0500

    update callback name

--- Lab2/naviSub.cpp
+++ Lab2/naviSub.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 float command[2];
 
-void print_msgs(const geometry_msgs::Twist::ConstPtr& msg) {
+void cmdUpdate(const geometry_msgs::Twist::ConstPtr& msg) {
 	command[0] = msg->linear.x;
 	command[1] = msg->angular.z;
 }
@@ -17,7 +17,7 @@ int main(int argc, char** argv) {
 	init(argc, argv, "navi");
 	geometry_msgs::Twist msg;
 	NodeHandle n;
-	Subscriber sub = n.subscribe("/cmd_vel_mux/input/navi",1000,print_msgs);
+	Subscriber sub = n.subscribe("/cmd_vel_mux/input/navi",1000,cmdUpdate);
 	Publisher pub = n.advertise<geometry_msgs::Twist>("navi", 1);
 	ros::Rate loop_rate(10);
 	while (ros::ok()) {
