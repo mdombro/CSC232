47ce9d0436aeff2585796dac9fb6491cdb0a7bed
Matthew <mdombros@u.rochester.edu>
2016-03-17 23:57:31 -0400

    Lab3 work

--- Lab3/ros_subscriber.cpp
+++ Lab3/ros_subscriber.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <ros/ros.h>
 #include "nav_msgs/Odometry.h"
+#include "sensor_msgs/LaserScan.h"
 
 using namespace std;
 using namespace ros;
@@ -21,11 +22,22 @@ void print_msgs(const nav_msgs::Odometry::ConstPtr& msg) {
 	twist.close();
 }
 
+void save_scan(const sensor_msgs::LaserScan::ConstPtr& msg) {
+	ofstream beams;
+	beams.open("Beams_data.txt", std::ofstream::out | std::ofstream::app);
+	int num = msg->ranges.size();
+	for (int i = 0; i < num-1; i++) {
+		beams << msg->ranges[i] << ", ";
+	}
+	beams << msg->ranges[num-1] << ", " << msg->angle_min << ", " << msg->angle_max << ", " << msg->angle_increment << ", " << msg->time_increment << ", " << msg->scan_time << ", " << msg->range_min << ", " << msg->range_max << endl;
+}
+
 int main(int argc, char** argv) {
 	init(argc, argv, "listener");
-	
+
 	NodeHandle n;
 	Subscriber sub = n.subscribe("/odom",1000,print_msgs);
+	Subscriber laser = n.subscribe("/scan", 1000, save_scan);
 	ros::Rate loop_rate(10);
 	ros::spin();
 	return 0;
